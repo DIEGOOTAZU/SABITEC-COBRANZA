b9b918afa185488031b95720acde5f5dc3c0068f
diego otazu <diegootazu@gmail.com>
2025-01-09 11:55:16 -0500

    pruebaaa0.2

--- config/db.php
+++ config/db.php
@@ -1,9 +1,9 @@
 <?php
 // Configuración de la base de datos PostgreSQL en Render
-$host = 'dpg-ctvb9epu0jms73b0j5d0-a'; // Reemplaza con el "Nombre de host" de Render
-$dbname = 'db_sabitec';               // Reemplaza con el "Base de datos" de Render
-$username = 'db_sabitec_user';        // Reemplaza con el "Nombre de usuario" de Render
-$password = 'Ruy8SL8VOIfCpLwS07etSc4BF7oBeJmP'; // Reemplaza con la "Contraseña" de Render
+$host = 'dpg-ctvvjb23esus73ad4qpg-a'; // Reemplaza con el "Nombre de host" de Render
+$dbname = 'gestion_cobros';               // Reemplaza con el "Base de datos" de Render
+$username = 'gestion_cobros_user';        // Reemplaza con el "Nombre de usuario" de Render
+$password = 'HFXdvU5CkBNTJkgbZjesELr7C7Wk3O6A'; // Reemplaza con la "Contraseña" de Render
 $port = 5432;                         // Reemplaza con el "Puerto" de Render
 
 try {
@@ -11,7 +11,7 @@ try {
     $conn = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $username, $password);
     // Configurar el modo de error de PDO para excepciones
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Conexión exitosa a PostgreSQL.";
+   // echo "Conexión exitosa a PostgreSQL.";
 } catch (PDOException $e) {
     // Manejo de errores en caso de falla de conexión
     die("Error de conexión: " . $e->getMessage());
